19a9db07dc93223a84e73dfee2fdecaf062f9fb0
Giulia Pizzignacco <be.giu@hotmail.com>
2016-10-22 18:05:57 +0100

    Added environment variables

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,30 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/*.coffee'];
-var jsSources = [
-	'components/scripts/*.js'
-];
+var env,
+	coffeeSources,
+	jsSources,
+	sassSources,
+	htmlSources,
+	jsonSources,
+	outputDir,
+	sassStyle;
 
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+env = process.env.NODE_ENV || 'development';
+
+if(env==='development'){
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+} else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/*.coffee'];
+jsSources = ['components/scripts/*.js'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 
 gulp.task('coffee', function() {
@@ -27,7 +43,7 @@ gulp.task('coffee', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe (gulp.dest('builds/development/js'))
+		.pipe (gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
 
@@ -35,11 +51,11 @@ gulp.task('compass', function() {
   gulp.src(sassSources)
     .pipe(compass({
       sass: 'components/sass',
-      image: 'builds/development/images',
-      style: 'expanded'
+      image: outputDir + 'images',
+      style: sassStyle
     })
     .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
+    .pipe(gulp.dest(outputDir + 'css'))
     .pipe(connect.reload())
 });
 
@@ -53,7 +69,7 @@ gulp.task('watch', function(){
 
 gulp.task('connect', function(){
 	connect.server({
-		root: 'builds/development/',
+		root: outputDir,
 		livereload: true
 	});
 });
